4389dc1f365655bb32d737b564368b0d9e03da97
Sleepyheadp <sleepyheadp322@gmail.com>
2024-04-05 17:17:25 +0800

    create an app with Nuxt3

---
+++ app.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Author: Sleepyheadp sleepyheadp322@gmail.com
+ * @Date: 2024-03-19 11:48:33
+ * @LastEditors: Sleepyheadp sleepyheadp322@gmail.com
+ * @LastEditTime: 2024-04-05 15:03:05
+ * @FilePath: /my_nuxtjs/app.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+	<div>
+		<!-- <NuxtWelcome /> -->
+		<div class="text-3xl font-bold underline">hello Nuxt.js</div>
+	</div>
+</template>
