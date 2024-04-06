d57cd0ccc3e9bc0f952d465217141ec849ede5f5
Sleepyheadp <sleepyheadp322@gmail.com>
2024-04-06 11:04:48 +0800

    layouts

---
+++ layouts/custom.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+	<div>
+		<slot></slot>
+		I am custom layout
+	</div>
+</template>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <script setup></script>
 <template>
 	<div>
+		<!-- 给所有默认的布局添加 header and footer -->
 		<AppHeader></AppHeader>
 		<slot></slot>
 		<AppFooter></AppFooter>
