0b9bf6ac6530852d29c34ea11f9474d76b326cb9
Sleepyheadp <sleepyheadp322@gmail.com>
2024-04-06 11:30:46 +0800

    Images, assets & public folder

--- components/profile/header/Avatar.vue
+++ components/profile/header/Avatar.vue
@@ -1,6 +1,20 @@
 <script setup></script>
 <template>
-	<div>I am profile's avatar</div>
+	<div>
+		<div class="title">I am profile's avatar</div>
+		<!-- 引入icons -->
+		<IconsBell></IconsBell>
+		<!-- 引入assets中的资源 -->
+		<img src="@/assets/1.jpg" alt="" class="avatar" />
+		<!-- 引入public中的资源 -->
+		<img src="/2.jpg" alt="" class="avatar" />
+		<!-- 如果要使用svg的话,要安装插件进行转译 : jjjjjjkkvite-svg-loader -->
+	</div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.avatar {
+	width: 100px;
+	height: 100px;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+	alias: {
+		assets: "/<rootDir>/assets",
+	},
 	css: ["~/assets/css/main.scss"],
 	postcss: {
 		plugins: {
